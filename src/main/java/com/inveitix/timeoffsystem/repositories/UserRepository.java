4e724b057ec67e2f4fc00d3401fc6aa70584a2b0
koksyncho <hrokishmokibarbaroki@gmail.com>
2017-11-03 22:27:42 +0200

    Uploading a TestServer template
    This test server was uploaded so that we can figure out how to run a Spring Boot application downloaded from our GitHub (some files are ignored through the upload process).

--- src/main/java/com/inveitix/timeoffsystem/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inveitix.timeoffsystem.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.inveitix.timeoffsystem.entities.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
